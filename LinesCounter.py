082e10cfebb24fb0d61295417cb1eb05fdf12c75
Raffaele Fiorillo <75253335+RaffaeleFiorillo@users.noreply.github.com>
2023-08-05 15:54:13 +0100

    Add files via upload

--- LinesCounter.py
+++ LinesCounter.py
@@ -1,45 +1,75 @@
 import os
 
-
-root = "C:\\Users\\Dark Knight\\Desktop\\Projects\\Games\\Fast_and_Curious"
-UNDESIRED = [root+"\src\deprecated",
-			 root+"\src\\testing_env",
-			 root+"\src\\tools"]
+root = os.path.abspath(os.getcwd())
+UNDESIRED = []
+user_chosen_extensions = []
 
 
-def count_lines(filename):
-	with open(filename, 'r') as file:
-		lines = file.readlines()
+def apply_configurations():
+	global root
+	global UNDESIRED
+	global user_chosen_extensions
+	try:
+		with open("LinesCounter-config.txt", 'r') as f:
+			lines = f.readlines()
+			for line in lines:
+				try:
+					exec(line.strip(), globals())
+				except Exception as e:
+					print(f"Error executing line: {line.strip()}")
+					print(f"Error message: {e}")
+	except SyntaxError:
+		print(f"Error: {SyntaxError}")
+
+	UNDESIRED = [root + d for d in UNDESIRED]
+	print(root)
+	print(UNDESIRED)
+	print(user_chosen_extensions)
+
+
+def count_lines(file_name):
+	with open(file_name, 'r') as f:
+		lines = f.readlines()
 		non_blank_lines = len([line for line in lines if line.strip()])
 		total_lines = len(lines)
 		return total_lines, non_blank_lines
 
 
-def count_lines_recursive(directory):
-	results = {}
-	root_non_blank_count = 0
-	root_total_count = 0
-	for root, dirs, files in os.walk(directory):
+def file_has_correct_extension(f):
+	global user_chosen_extensions
+	for extension in user_chosen_extensions:
+		if f.endswith(extension):
+			return True
+	return False
+
+
+def count_lines_recursive(directory_used):
+	global UNDESIRED
+	res = {}
+	r_non_blank_count = 0
+	r_total_count = 0
+	for root, dirs, files in os.walk(directory_used):
 		if root in UNDESIRED:
 			continue
-		root_path = os.path.relpath(root, directory)
+		root_path = os.path.relpath(root, directory_used)
 		root_count = [0, 0]
 		for file in files:
 			filepath = os.path.join(root, file)
-			if file.endswith('.py'): # and filepath not in UNDESIRED:
+			if file_has_correct_extension(file):
 				count = count_lines(filepath)
 				root_count[0] += count[0]
 				root_count[1] += count[1]
 				file_path = os.path.join(root_path, file)
-				results[file_path] = count
+				res[file_path] = count
 		if root_count != [0, 0]:
-			results[root_path] = tuple(root_count)
-		root_non_blank_count += root_count[1]
-		root_total_count += root_count[0]
-	return results, root_non_blank_count, root_total_count
+			res[root_path] = tuple(root_count)
+		r_non_blank_count += root_count[1]
+		r_total_count += root_count[0]
+	return res, r_non_blank_count, r_total_count
 
 
 if __name__ == '__main__':
+	apply_configurations()
 	directory = os.getcwd()
 	results, root_non_blank_count, root_total_count = count_lines_recursive(directory)
 	with open('code_line_counts.txt', 'w') as file:
